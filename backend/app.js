91c3aeb5b666a2ebc59f73945df5e3ad62f6641a
PaulAjii <paulajii134@gmail.com>
2022-11-15 06:34:02 +0100

    Slight change to app

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(logger)
 
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).json({msg: 'Welcome to the app!!!'})
+  res.status(200).json({ msg: 'Welcome to the app!!!' })
 })
 
 // lidtening
