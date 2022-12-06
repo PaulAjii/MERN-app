a4a80d36319a185713510e8d200fa05d420951b4
PaulAjii <paulajii134@gmail.com>
2022-12-06 03:52:06 +0100

    Json Web Token

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,4 +1,9 @@
 const User = require('../models/Users')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' })
+}
 
 const loginUser = async (req, res) => {
   res.send('User Loged in!')
@@ -9,7 +14,10 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password)
-    res.status(201).json({ email, user })
+
+    const token = createToken(user._id)
+
+    res.status(201).json({ email, token })
   } catch (error) {
     res.status(400).json({
       error: error.message
