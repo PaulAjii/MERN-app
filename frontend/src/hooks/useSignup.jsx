46b474c16754cf3dd7d64bd4420891d229f48465
Paul Ajijola <86701149+PaulAjii@users.noreply.github.com>
2024-02-18 13:28:10 +0100

    Update useSignup.jsx

--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -11,7 +11,7 @@ export const useSignup = () => {
   setIsLoading(true)
   setError(null)
 
-  const response = await fetch('http://localhost:5000/api/v1/users/signup', {
+  const response = await fetch('https://exercise-reps.onrender.com/api/v1/users/signup', {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({ email, password })
